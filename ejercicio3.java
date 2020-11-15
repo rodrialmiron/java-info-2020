ad349cda8faf397a2d9ce509b04991c13ce37207
rodrialmiron <rodri.22.10.14@gmail.com>
2020-11-15 10:11:05 -0300

    mi primer commit

---
+++ ejercicio3.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class ejercicio3{
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int nota = scan.nextInt();
+        if (nota > 92){
+            System.out.println("Excelente");
+        } else if (nota > 84){
+            System.out.println("Sobresaliente");
+        } else if (nota > 74){
+            System.out.println("Distinguido");    
+        } else if (nota > 64){
+            System.out.println("Bueno");
+        }else{
+            System.out.println("Desaprobado");
+        }
+    }
+}
---
+++ ejercicio4.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class ejercicio4{
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int num = scan.nextInt();
+        if (num == 1){
+            System.out.println("Hoy es Domingo");
+        } else if (num == 2){
+            System.out.println("Hoy es Lunes");
+        } else if (num == 3){
+            System.out.println("Hoy es Martes");
+        } else if (num == 4){
+            System.out.println("Hoy es Miercoles");
+        } else if (num == 5){
+            System.out.println("Hoy es Jueves");
+        } else if (num == 6){
+            System.out.println("Hoy es Viernes");
+        } else if (num == 7){
+            System.out.println("Hoy es Sabado");
+        } else{
+            System.out.println("Ingrese un numero del 1 al 7");
+        }
+    }
+}
